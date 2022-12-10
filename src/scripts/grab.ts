5989fa2fe3e98334053a12f693ef90121f61da05
lszlo <33127492+lszlo@users.noreply.github.com>
2022-12-10 22:42:44 +0200

    dd

--- src/scripts/grab.ts
+++ src/scripts/grab.ts
@@ -21,7 +21,7 @@ instance.interceptors.response.use(
 
 
 type assetsType = "loot" | "summoner-emotes" | "summoner-icon-sets" | "summoner-icons" | "ward-skin-sets" | "ward-skins";
-type langType =  "english" | "chinese" | "spanish" ;
+type langType =  "english" | "chinese" ;
 let langMap = new Map([
   //["chinese", "zh_cn"],
   ["english", "default"]
